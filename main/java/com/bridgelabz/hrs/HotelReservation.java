c9bc430693799e44cd0c1965c1b1ec4666532520
Saravana20-S <iamsaravanan1347@gmail.com>
2026-07-18 09:40:00 +0530

    Added: Display welcome message for Hotel Reservation Program

---
+++ main/java/com/bridgelabz/hrs/HotelReservation.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.hrs;
+
+/**
+ * HotelReservation class is the entry point of the Hotel Reservation System.
+ * It displays a welcome message to the user.
+ *
+ * UC1: Display Welcome to Hotel Reservation Program.
+ *
+ * @author Saravanan
+ */
+public class HotelReservation {
+
+    /**
+     * Main method - execution starts from here.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("Welcome to Hotel Reservation Program");
+
+    }
+}
